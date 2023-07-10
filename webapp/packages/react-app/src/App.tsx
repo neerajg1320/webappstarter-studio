be5b95e7d77a6f76c5607b12b4c6fa19fef6bd28
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-07-10 11:11:01 +0530

    The currentProject state is wired and it working

--- webapp/packages/react-app/src/App.tsx
+++ webapp/packages/react-app/src/App.tsx
@@ -1,6 +1,16 @@
 import CellList from "./components/cell-list/cell-list";
+import { useActions } from "./hooks/use-actions";
+import { useTypedSelector } from "./hooks/use-typed-selector";
 
 const App = () => {
+  const {setCurrentProject} = useActions();
+  // const projectsState = useTypedSelector((state) => state.projects);
+
+  const handleInputChange = (value:string) => {
+    setCurrentProject(value, "reactjs");
+    // console.log(projectsState);
+  };
+
   return (
     <div style={{display:"flex", flexDirection:"column", alignItems: "center"}}>
       <div style={{
@@ -9,7 +19,7 @@ const App = () => {
         }}
       >
         <label>Project</label>
-        <input type="text"/>
+        <input type="text" onChange={(e) => {handleInputChange(e.target.value)}} />
       </div>
       <div style={{
           // border: "solid 2px white", 
