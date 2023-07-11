5f087c0632bfeb2a7432b2d98acede6841e37327
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-07-11 15:52:18 +0530

    Changed the project code to use sync

--- webapp/packages/react-app/src/components/project/project.tsx
+++ webapp/packages/react-app/src/components/project/project.tsx
@@ -5,24 +5,28 @@ import {useTypedSelector} from "../../hooks/use-typed-selector";
 import {randomIdGenerator} from "../../state/id";
 
 const Project:React.FC = () => {
-  const [projectName, setProjectName] = useState('');
+  const [projectName, setProjectName] = useState<string|null>(null);
   const { createAndSetProject, updateProject, createProjectBundle} = useActions();
   const projectsState = useTypedSelector((state) => state.projects);
   const bundlesState =  useTypedSelector((state) => state.bundles);
 
   // console.log('Project: rendered', JSON.stringify(projectsState));
 
-  const proj = useMemo(() => {
+  const project = useMemo(() => {
     if (Object.keys(projectsState.data).length > 0) {
       return Object.entries(projectsState.data)[0][1];
     }
     return null;
   }, [projectsState]);
 
-  console.log('Project: rendered', JSON.stringify(proj));
+  console.log('Project: rendered', JSON.stringify(project));
 
-  const handleSaveClick = () => {
-    if (!proj) {
+  const handleSyncClick = () => {
+    if (!projectName) {
+      console.error(`Project name is required`)
+      return;
+    }
+    if (!project) {
       const localId = randomIdGenerator();
       createAndSetProject(localId, projectName, "reactjs");
     } else {
@@ -32,9 +36,9 @@ const Project:React.FC = () => {
   }
 
   const handleBundleClick = () => {
-    if (proj) {
+    if (project) {
       // TBD: The project starting file is assumed to be index.js, we will soon add a check
-      createProjectBundle(proj.localId, `${proj.name}/index.js`);
+      createProjectBundle(project.localId, `${project.name}/index.js`);
     }
   }
 
@@ -53,14 +57,15 @@ const Project:React.FC = () => {
         >
           <div style={{display: "flex", gap: "20px"}}>
             <label>Project</label>
-            <input type="text" value={projectName} onChange={(e) => {setProjectName(e.target.value)}} />
+            <input type="text" value={projectName||''} onChange={(e) => {setProjectName(e.target.value)}} />
           </div>
           <div>
             <button
                 className="button is-primary is-small"
-                onClick={handleSaveClick}
+                onClick={handleSyncClick}
+                disabled={!projectName}
             >
-              Save
+              Sync
             </button>
           </div>
         </div>
@@ -69,6 +74,7 @@ const Project:React.FC = () => {
           <button
               className="button is-family-secondary is-small"
               onClick={handleBundleClick}
+              disabled={!(project && project.synced)}
           >
             Bundle
           </button>
@@ -77,10 +83,10 @@ const Project:React.FC = () => {
       </div>
 
 
-      {(proj && bundlesState[proj.localId]) &&
+      {(project && bundlesState[project.localId]) &&
           <div>
             {/*<pre>{bundlesState[projectId]!.code}</pre>*/}
-            <Preview code={bundlesState[proj.localId]!.code} err={bundlesState[proj.localId]!.err}/>
+            <Preview code={bundlesState[project.localId]!.code} err={bundlesState[project.localId]!.err}/>
           </div>
       }
     </>
