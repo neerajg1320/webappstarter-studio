b7655c1ffd6a4021a9fd87a7e2e14026d7d79849
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-07-18 15:32:45 +0530

    Added control tree bar

--- webapp/packages/react-app/src/components/files-tree/file-tree-control-bar.tsx
+++ webapp/packages/react-app/src/components/files-tree/file-tree-control-bar.tsx
@@ -4,11 +4,26 @@ import {ReduxProject} from "../../state";
 
 interface FileTreeControlBarProps {
   reduxProject: ReduxProject;
+  onEvent: () => {};
 }
-const FileTreeControlBar:React.FC<FileTreeControlBarProps> = ({reduxProject}) => {
+
+const FileTreeControlBar:React.FC<FileTreeControlBarProps> = ({reduxProject, onEvent}) => {
+  const handleCreateFile: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    console.log("We need to add file");
+  }
+
   return (
       <div className="file-tree-control-bar">
-
+        <button className="button is-family-secondary is-small" onClick={handleCreateFile}>
+          <span className="icon">
+              <i className="fas fa-plus" />
+          </span>
+        </button>
+        <button className="button is-family-secondary is-small" onClick={() => {}}>
+          <span className="icon">
+              <i className="fas fa-copy" />
+          </span>
+        </button>
       </div>
   );
 }
