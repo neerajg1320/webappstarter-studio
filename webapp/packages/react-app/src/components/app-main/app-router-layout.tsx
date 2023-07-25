2b637e41f093367c2416ed73da1e2398773fc808
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-07-25 18:29:29 +0530

    App working after disabled code

--- webapp/packages/react-app/src/components/app-main/app-router-layout.tsx
+++ webapp/packages/react-app/src/components/app-main/app-router-layout.tsx
@@ -1,27 +1,24 @@
-import './app-router-layout.css';
-import {Outlet, Link, useNavigate} from "react-router-dom";
-import {useTypedSelector} from "../../hooks/use-typed-selector";
-import {useActions} from "../../hooks/use-actions";
-import {RouteName} from "../routes";
+// import './app-router-layout.css';
+import {Outlet} from "react-router-dom";
 import AppNavBar from "./app-nav-bar";
 
 const AppRouterLayout = () => {
 
   return (
-      <div style={{
-          height: "80vh",
-          width:"100%", padding:"0 40px",
-          display: "flex", flexDirection:"column", gap:"20px"
-        }}
-      >
-        <AppNavBar />
-
-        <div className="outlet app-router-view">
-          <Outlet />
-        </div>
-
-      </div>
+      <h1>Not to be used</h1>
+      // <div style={{
+      //     height: "80vh",
+      //     width:"100%", padding:"0 40px",
+      //     display: "flex", flexDirection:"column", gap:"20px"
+      //   }}
+      // >
+      //   <AppNavBar />
+      //
+      //   {/*<div className="outlet app-router-view">*/}
+      //   {/*  <Outlet />*/}
+      //   {/*</div>*/}
+      //
+      // </div>
   )
 };
 
-export default AppRouterLayout;
