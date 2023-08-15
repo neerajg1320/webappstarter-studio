40cbea5eb16c7e4af98018b376bbd47b7a7995d7
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-15 11:06:01 +0530

    Added style to loading indicator

--- webapp/packages/react-app/src/components/common/loading-indicator.tsx
+++ webapp/packages/react-app/src/components/common/loading-indicator.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const LoadingIndicator = () => {
   return (
-      <div>
+      <div style={{
+          height: "100%", width: "100%",
+          display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"
+        }}
+      >
         <span>Loading...</span>
       </div>
   );
