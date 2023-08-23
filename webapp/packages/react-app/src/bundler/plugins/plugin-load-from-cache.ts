11574b17682493eb530b93f45508167e9545b445
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-23 12:02:18 +0530

    Fixed the comments for debugPlugin

--- webapp/packages/react-app/src/bundler/plugins/plugin-load-from-cache.ts
+++ webapp/packages/react-app/src/bundler/plugins/plugin-load-from-cache.ts
@@ -8,6 +8,9 @@ if (cacheEnabled) {
   fileCache = localforage.createInstance({
       name: 'fileCache'
   });
+  if (debugCache) {
+    console.log(`Initialized Cache on IndexDB`);
+  }
 }
 
 export const pluginLoadFromCache = () => {
@@ -18,13 +21,12 @@ export const pluginLoadFromCache = () => {
       // This checks if the file is in the cache. If present then returns the cached result.
       // If not then it returns nothing. This forces the esbuild to look at subsequent onLoad handlers.
       build.onLoad({filter: /.*/}, async (args: any) => {
-
         // If we have already fetched this file then return from cache
         // We use args.path as key in the cache
         if (cacheEnabled) {
           const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
           if (cachedResult) {
-            if (debugCache) {
+            if (debugPlugin) {
               console.log(`loaded ${args.path} from cache`);
             }
             return cachedResult;
@@ -39,7 +41,7 @@ export const setFileInCache = async (file:string, contents:esbuild.OnLoadResult)
   if (cacheEnabled) {
     // Store result in cache
     await fileCache.setItem(file, contents);
-    if (debugCache) {
+    if (debugPlugin) {
       console.log(`stored ${file} to cache`);
     }
   }
