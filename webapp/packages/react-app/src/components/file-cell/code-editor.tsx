c398e6c9c6cbdd0728c73d1cb7b718110effbea3
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-27 12:22:53 +0530

    Removed the reference of window from file

--- webapp/packages/react-app/src/components/file-cell/code-editor.tsx
+++ webapp/packages/react-app/src/components/file-cell/code-editor.tsx
@@ -2,14 +2,8 @@ import './code-editor.css';
 import './syntax.css';
 import React, {useEffect, useMemo, useRef} from 'react';
 import MonacoEditor, {OnMount} from '@monaco-editor/react';
-// https://stackoverflow.com/questions/70538511/you-may-need-an-additional-loader-to-handle-the-result-of-these-loaders-upgradi
-// import * as monaco from 'monaco-editor';
-// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import prettier from 'prettier';
 import parserBabel from 'prettier/parser-babel';
-import codeShift from 'jscodeshift';
-import Highlighter from 'monaco-jsx-highlighter';
-import {debugComponent} from "../../config/global";
 import {CodeLanguage} from "../../state/language";
 
 // https://stackoverflow.com/questions/30239060/uncaught-referenceerror-process-is-not-defined
@@ -23,6 +17,24 @@ interface CodeEditorProps {
   language: CodeLanguage;
   onChange?: (value:string) => void | null;
   disabled?: boolean;
+}
+
+const configTypescript = (monaco:any) => {
+  // https://stackoverflow.com/questions/56954280/monaco-editor-how-to-disable-errors-typescript
+  // https://blog.expo.dev/building-a-code-editor-with-monaco-f84b3a06deaf
+  // We will set the language settings for typescript
+  try {
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: true,
+      // diagnosticCodesToIgnore: [
+      //   2792, // Unused imports
+      //   2304, 1005, 1161, // For JSX
+      // ]
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // This function is used to active the JSX syntax highlighting
@@ -49,6 +61,7 @@ const activateMonacoJSXHighlighter = async (monacoEditor:any, monaco:any) => {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, language, onChange, disabled}) => {
+  const debugComponent = true;
   const editorRef = useRef<any>();
   const editorLanguage = useMemo(() => {
     if (language === CodeLanguage.JAVASCRIPT) {
@@ -63,7 +76,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, language, onChange
 
     return 'javascript';
   }, [language]);
-  
+
   useEffect(() => {
     if (debugComponent) {
       console.log(`CodeEditor: useEffect([]) created`);
@@ -83,16 +96,13 @@ const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, language, onChange
   const handleEditorMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
 
+    activateMonacoJSXHighlighter(editor, monaco);
+
     // Note the initial value in the listener is always blank. It doesn't get updated on rerenders
     editor.onDidChangeModelContent((e) => {
       // https://github.com/suren-atoyan/monaco-react#usemonaco
-
-
       const newValue = editorRef.current.getValue();
 
-
-      // console.log(`initialValue: ${initialValue}`);
-      // console.log(`newValue: ${newValue}`);
       if (onChange) {
         onChange(newValue);
       }
@@ -104,23 +114,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, language, onChange
       model.updateOptions({tabSize: 2});
     }
 
-
-    // https://stackoverflow.com/questions/56954280/monaco-editor-how-to-disable-errors-typescript
-    // https://blog.expo.dev/building-a-code-editor-with-monaco-f84b3a06deaf
-    // We will set the language settings for typescript
-    try {
-      // @ts-ignore
-      window.monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-        noSemanticValidation: true,
-        noSyntaxValidation: true,
-        // diagnosticCodesToIgnore: [
-        //   2792, // Unused imports
-        //   2304, 1005, 1161, // For JSX
-        // ]
-      });
-    } catch (err) {
-      console.error(err);
-    }
+    configTypescript(monaco);
   };
 
   const handleFormatClick = () => {
