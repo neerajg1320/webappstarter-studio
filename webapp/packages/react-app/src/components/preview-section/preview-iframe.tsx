da9b9957be6e1d63a73ddad3051c82bc227941da
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-29 11:09:00 +0530

    The injection of parentCommunicationJavscriptCode is successful

--- webapp/packages/react-app/src/components/preview-section/preview-iframe.tsx
+++ webapp/packages/react-app/src/components/preview-section/preview-iframe.tsx
@@ -1,48 +1,55 @@
 import './preview-iframe.css';
 import {useEffect, useRef, useState} from "react";
 
+const simpleJavascriptCode = `
+    console.log('hello from script');
+    console.log('would you talk to me');
+`;
+
 const parentCommunicationJavascriptCode = `
-      const window_console_log = window.console.log;
+    const window_console_log = window.console.log;
+    
+    window.console.log = function(...args) {
+      // We save the console.log function before we override it
+      window_console_log(...args);
       
-      window.console.log = function(...args) {
-        // We save the console.log function before we override it
-        window_console_log(...args);
-        
-        const message = {
-          source: "iframe",
-          type: 'log',
-          content: args, // args is an array
-        }
-        window.parent.postMessage(message, '*');
+      const message = {
+        source: "iframe",
+        type: 'log',
+        content: args, // args is an array
       }
+      window.parent.postMessage(message, '*');
+    }
+    
+    // TBD: Need to verify the flow here
+    const handleError = (err) => {
+      const root = document.querySelector('#root');
+      root.innerHTML = '<div style="color:red"><h4>Runtime Error:</h4>' + err + '</div>';
+      // console.error(err);
       
-      // TBD: Need to verify the flow here
-      const handleError = (err) => {
-        const root = document.querySelector('#root');
-        root.innerHTML = '<div style="color:red"><h4>Runtime Error:</h4>' + err + '</div>';
-        // console.error(err);
-        
-        const message = {
-          source: "iframe",
-          type: 'error',
-          content: err,
-        }
-        window.parent.postMessage(message, '*');        
-      };
+      const message = {
+        source: "iframe",
+        type: 'error',
+        content: err,
+      }
+      window.parent.postMessage(message, '*');        
+    };
 
-      window.addEventListener('error', (event) => {
-        event.preventDefault();
-        handleError(event.error);
-      });
+    window.addEventListener('error', (event) => {
+      event.preventDefault();
+      handleError(event.error);
+    });
 
-      window.addEventListener('message', (event) => {
-        // console.log(event.data);
-        try {
-          eval(event.data);
-        } catch (err) {
-          handleError(err);
-        }
-      }, false);
+    window.addEventListener('message', (event) => {
+      // console.log(event.data);
+      try {
+        eval(event.data);
+      } catch (err) {
+        handleError(err);
+      }
+    }, false);
+    
+    console.log('Parent Communicated Script Injected');
 `;
 
 const htmlWithScript = `
@@ -78,44 +85,36 @@ const htmlNoScript = `
 </html>
 `;
 
+const injectScriptInHtml = (markupStr:string, javscriptCodeStr:string):string => {
+  let resultMarkupStr = "Not processed";
+
+  // https://stackoverflow.com/questions/10585029/parse-an-html-string-with-js
+  var detachedHtml = document.createElement( 'html' );
+  detachedHtml.innerHTML = htmlNoScript;
+
+  const bodyEl = detachedHtml.getElementsByTagName('body');
+  if (bodyEl.length > 0) {
+    const scriptEl = document.createElement('script');
+    scriptEl.innerHTML = javscriptCodeStr;
+
+    bodyEl[0].appendChild(scriptEl);
+  }
+
+  resultMarkupStr = detachedHtml.innerHTML;
+
+  return resultMarkupStr;
+}
 
 const PreviewIframe:React.FC<PreviewProps> = ({code, err}) => {
   const iframeRef = useRef<any>();
   const [markup, setMarkup] = useState<string>("");
 
-  const parseHtml = (markupStr:string):string => {
-    let resultMarkupStr = "Not processed";
-
-    var detachedHtml = document.createElement( 'html' );
-    // el.innerHTML = "<html><head><title>titleTest</title></head><body><a href='test0'>test01</a><a href='test1'>test02</a><a href='test2'>test03</a></body></html>";
-    // const elements = el.getElementsByTagName( 'a' ); // Live NodeList of your anchor elements
-    detachedHtml.innerHTML = htmlNoScript;
-
-    const titleEl = detachedHtml.getElementsByTagName('title');
-    if (titleEl.length > 0) {
-      setMarkup(titleEl[0].innerText);
-    }
-
-    const bodyEl = detachedHtml.getElementsByTagName('body');
-    if (bodyEl.length > 0) {
-      const scriptEl = document.createElement('script');
-      scriptEl.innerText = "console.log('hello from script')"
-
-      bodyEl[0].appendChild(scriptEl);
-    }
-
-    resultMarkupStr = detachedHtml.innerHTML;
-    setMarkup(resultMarkupStr);
-
-    return resultMarkupStr;
-  }
-
   useEffect(() => {
-    const htmlWithInjectedScript = parseHtml(htmlNoScript);
+    const htmlWithInjectedScript = injectScriptInHtml(htmlNoScript, parentCommunicationJavascriptCode);
+    setMarkup(htmlWithInjectedScript);
 
-    iframeRef.current.srcdoc = htmlWithScript;
-    // iframeRef.current.srcdoc = htmlWithInjectedScript;
-
+    // iframeRef.current.srcdoc = htmlWithScript;
+    iframeRef.current.srcdoc = htmlWithInjectedScript;
 
     // To make it fool proof we should convert it to be dependent on message from iframe
     setTimeout(() => {
