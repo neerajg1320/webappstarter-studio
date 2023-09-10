6f46f3f93636b2865219c5338151cfd11b8a7e0f
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-09-11 01:45:47 +0530

    Fixed the polyfill issue for libs and global vars

--- webapp/packages/studio-app/vite.config.ts
+++ webapp/packages/studio-app/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      nodePolyfills({
+        // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+        include: ['path'],
+        // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
+        exclude: [
+          'fs', // Excludes the polyfill for `fs` and `node:fs`.
+        ],
+        // Whether to polyfill specific globals.
+        globals: {
+          Buffer: true, // can also be 'build', 'dev', or false
+          global: true,
+          process: true,
+        },
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true,
+      }),
+  ],
 })
