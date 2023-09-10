ca877baac08f8278562e26b1a8dfbe98af7ea7d4
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-09-11 01:20:19 +0530

    Added bulmaswatch and fortawesome

--- webapp/packages/studio-app/src/main.tsx
+++ webapp/packages/studio-app/src/main.tsx
@@ -1,3 +1,5 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
