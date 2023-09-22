326e722f7edbf9b546f7bd8e90c69c9fd6d87f23
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-09-22 16:06:45 +0530

    Moved the New to the List viewg

--- webapp/packages/studio-app/src/components/project-resource/project-list/project-list-grid/project-list-grid.tsx
+++ webapp/packages/studio-app/src/components/project-resource/project-list/project-list-grid/project-list-grid.tsx
@@ -3,10 +3,7 @@ import {useNavigate} from "react-router-dom";
 import {useTypedSelector} from "../../../../hooks/use-typed-selector";
 import ProjectListItemCard from "./project-list-item-card";
 import {debugComponent, debugProject} from "../../../../config/global";
-import {RoutePath} from "../../../routes";
-import {generateLocalId} from "../../../../state/id";
 import {useActions} from "../../../../hooks/use-actions";
-import {ProjectFrameworks, ProjectTemplates, ReactToolchains} from "../../../../state";
 import './project-list-grid.css';
 
 // This component shows us the projects available for a user
@@ -20,7 +17,7 @@ const ProjectListGrid:React.FC<ProjectListGridProps> = ({visibility:propVisibili
   // const debugComponent = true;
   const navigate = useNavigate();
   const isAuthenticated = useTypedSelector<boolean>(state => state.auth.isAuthenticated);
-  const {createAndSetProject, fetchProjectsAndFiles} = useActions();
+  const {fetchProjectsAndFiles} = useActions();
 
   const projectsState = useTypedSelector((state) => state.projects);
   const projectList = useMemo(() => {
@@ -47,20 +44,6 @@ const ProjectListGrid:React.FC<ProjectListGridProps> = ({visibility:propVisibili
     }
   }, []);
 
-
-  const handleNewProjectClick = () => {
-    const localId = generateLocalId();
-    createAndSetProject({
-      localId,
-      title: `Project-${localId}`,
-      description: 'This is a web application',
-      template: ProjectTemplates.JAVASCRIPT_WITH_CSS,
-      framework: ProjectFrameworks.NONE,
-      toolchain: ReactToolchains.NONE
-    });
-    navigate(RoutePath.PROJECT_NEW);
-  }
-
   return (
       <div className="project-list-grid" style={{
         width: "100%",
@@ -72,25 +55,7 @@ const ProjectListGrid:React.FC<ProjectListGridProps> = ({visibility:propVisibili
       }}
       >
         <div style={{
-          height: "20%",
-          width: "100%",
-          padding: "0 20px",
-          display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-end"
-        }}>
-          <div style={{
-            borderRadius: "15px",
-            display: "flex",
-          }}>
-            <button className="button is-family-secondary"
-                    style={{backgroundColor: "cornflowerblue", borderRadius: "5px"}}
-                    onClick={handleNewProjectClick}
-            >
-              New Project
-            </button>
-          </div>
-        </div>
-        <div style={{
-          height: "80%",
+          height: "100%",
           width: "100%",
           // border: "1px solid lightblue",
           borderRadius: "15px",
--- webapp/packages/studio-app/src/components/project-resource/project-list/project-list-view.tsx
+++ webapp/packages/studio-app/src/components/project-resource/project-list/project-list-view.tsx
@@ -1,14 +1,42 @@
-import {useState} from "react";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import ProjectListScroller from "./project-list-scroller/project-list-scroller";
 import ProjectListGrid from "./project-list-grid/project-list-grid";
 import './project-list-view.css';
+import {generateLocalId} from "../../../state/id";
+import {RoutePath} from "../../routes";
+import {useActions} from "../../../hooks/use-actions";
+import {ProjectFrameworks, ProjectTemplates, ReactToolchains} from "../../../state";
 
 const ProjectListView = () => {
   const [isPreviewLayout, setPreviewLayout] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const {createAndSetProject} = useActions();
+
+  const handleNewProjectClick = () => {
+    const localId = generateLocalId();
+    createAndSetProject({
+      localId,
+      title: `Project-${localId}`,
+      description: 'This is a web application',
+      template: ProjectTemplates.JAVASCRIPT_WITH_CSS,
+      framework: ProjectFrameworks.NONE,
+      toolchain: ReactToolchains.NONE
+    });
+    navigate(RoutePath.PROJECT_NEW);
+  }
 
   return (
       <div className="project-list-view">
         <div className="project-list-view-control-bar">
+          <div style={{
+            borderRadius: "15px",
+            display: "flex",
+          }}>
+            <button className="button is-family-secondary" style={{backgroundColor: "cornflowerblue", borderRadius: "5px"}} onClick={handleNewProjectClick}>
+              New
+            </button>
+          </div>
           <div className="project-list-view-control-bar-setting">
             <label>Preview Mode</label>
             <input type="checkbox" checked={isPreviewLayout} onChange={(e) => setPreviewLayout(e.target.checked)}/>
