fa306f97900e758163a39965a2c7cf4cfb265e11
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-09-24 19:28:57 +0530

    Added template format for Props

--- webapp/packages/studio-app/src/hoc/logger.tsx
+++ webapp/packages/studio-app/src/hoc/logger.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useCallback} from 'react';
 
 
-export const withLifecyleLogger =  (InnerComponent:React.FC, debug=true):React.FC => {
+export const withLifecyleLogger = <PROPS extends {},> (InnerComponent:React.FC<PROPS>, debug=true):React.FC<PROPS> => {
   // Create the outer component
-  const WithLifeCycleLogger = (props) => {
+  const WithLifeCycleLogger:React.FC<PROPS> = (props) => {
     const renderCountRef = useRef<number>(0);
 
     const logEvent = useCallback((message) => {
