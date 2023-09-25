f82564a0b0b63ca9e5551123ba0c4bdc2f6c3017
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-09-25 14:42:52 +0530

    Changed the vite settings to set the port to 3000 as specified in .env.local

--- webapp/packages/studio-app/vite.config.ts
+++ webapp/packages/studio-app/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default (({mode}) => {
+  // https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '' )};
+  console.log(`process.env.VITE_PORT: ${process.env.VITE_PORT}`);
+
+  return defineConfig({
+    plugins: [
       react(),
       nodePolyfills({
         // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
@@ -24,5 +29,9 @@ export default defineConfig({
         protocolImports: true,
       }),
       // splitVendorChunkPlugin(),
-  ],
-})
+    ],
+    server: {
+      port: parseInt(process.env.VITE_PORT)
+    }
+  });
+});
