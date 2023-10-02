a01eff0c73599cfd337bac98908bb7d79679ca8f
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-10-03 00:06:52 +0530

    Enabled button for upload file

--- webapp/packages/studio-app/src/components/file-cell/file-cell-control-bar.tsx
+++ webapp/packages/studio-app/src/components/file-cell/file-cell-control-bar.tsx
@@ -104,7 +104,7 @@ const FileCellControlBar:React.FC<FileCellControlBarProps> = ({reduxFile}) => {
             </div>
         }
 
-        {isAdmin &&
+        {
             <div style={{display: "flex", flexDirection: "column", gap: "5px", alignItems: "center"}}>
               <button
                   className="button is-family-primary is-small"
@@ -112,7 +112,7 @@ const FileCellControlBar:React.FC<FileCellControlBarProps> = ({reduxFile}) => {
                     selectFileInputRef.current!.click()
                   }}
               >
-                Select File
+                Upload File
               </button>
               <input ref={selectFileInputRef} type="file" style={{display: "none"}} onChange={handleFileChange}/>
             </div>
