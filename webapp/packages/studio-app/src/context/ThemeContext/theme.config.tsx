7fbe50c21403f80260553afb14cab6374715e2a7
Vanshika <vanshika06065@gmail.com>
2023-12-29 19:10:26 +0530

    changed the backgroundColor in dark mode

--- webapp/packages/studio-app/src/context/ThemeContext/theme.config.tsx
+++ webapp/packages/studio-app/src/context/ThemeContext/theme.config.tsx
@@ -9,13 +9,15 @@ export const theme: Record<ThemeType, Theme> = {
     "--secondaryColor": Color.WHITE,
     "--toastErrorColor": Color.OFFRED,
     "--toastSuccessColor": Color.OFFBLUE,
-    "--shadowColor": Color.LIGHT_SHADOW
+    "--shadowColor": Color.LIGHT_SHADOW,
+    "--backgroundColor": Color.WHITE
   },
   dark: {
     "--primaryColor": Color.WHITE,
     "--secondaryColor": Color.BLACK,
     "--toastErrorColor": Color.OFFRED,
     "--toastSuccessColor": Color.OFFWHITE,
-    "--shadowColor": Color.DARK_SHADOW
+    "--shadowColor": Color.DARK_SHADOW,
+    "--backgroundColor": Color.DARK_BACKGROUND_COLOR
   },
 };
--- webapp/packages/studio-app/src/context/ThemeContext/theme.model.tsx
+++ webapp/packages/studio-app/src/context/ThemeContext/theme.model.tsx
@@ -6,11 +6,12 @@ export interface Theme {
   "--toastSuccessColor": Color;
   "--toastErrorColor": Color;
   "--shadowColor": Color;
+  "--backgroundColor": Color;
   // '--'
 }
 
 export enum Color {
-  BLACK = "#1A1110",
+  BLACK = "#1a1919",
   LIGHT_BLACK = "#251f1f",
   WHITE = "#FFFFFF",
   BLUE = "#2b4172",
@@ -18,6 +19,7 @@ export enum Color {
   OFFWHITE = "rgb(255 255 255 / 81%)",
   OFFRED = "rgb(231 76 60 / 90%)",
   LIGHT_SHADOW = "rgb(114 114 114 / 22%)",
-  DARK_SHADOW = "black"
+  DARK_SHADOW = "black",
+  DARK_BACKGROUND_COLOR = "#0c1216d6"
 
 }
