c7309f569fd98e13799d3c0823bc6e3ac80195fa
Vanshika <vanshika06065@gmail.com>
2024-01-20 15:07:12 +0530

    added toLowerCase() in getSearchProject

--- webapp/packages/studio-app/src/state/helpers/project-helpers.ts
+++ webapp/packages/studio-app/src/state/helpers/project-helpers.ts
@@ -1,43 +1,52 @@
-import {ProjectsState, ProjectMap, ReduxProject} from "../project";
+import { ProjectsState, ProjectMap, ReduxProject } from "../project";
 
+export const getProjects = (state: ProjectsState): ReduxProject[] => {
+  return Object.entries(state.data)
+    .map(([k, v]) => v)
+    .filter((item) => item.confirmed);
+};
 
-export const getProjects = (state:ProjectsState):ReduxProject[] => {
-  return Object.entries(state.data).map(([k,v]) => v).filter(item => item.confirmed);
-}
-
-export const getProjectFromLocalId = (state:ProjectsState, localId:string):ReduxProject => {
+export const getProjectFromLocalId = (
+  state: ProjectsState,
+  localId: string
+): ReduxProject => {
   return state.data[localId];
-}
+};
 
-export const getProjectFromServerId = (state:ProjectsState, serverId:number):ReduxProject|null => {
-  const matches = getProjects(state).filter(item => item.pkid == serverId);
+export const getProjectFromServerId = (
+  state: ProjectsState,
+  serverId: number
+): ReduxProject | null => {
+  const matches = getProjects(state).filter((item) => item.pkid == serverId);
   if (matches.length > 0) {
     return matches[0];
   }
   return null;
-}
+};
 
-export const getProjectEntryPath = (reduxProject:ReduxProject):string => {
+export const getProjectEntryPath = (reduxProject: ReduxProject): string => {
   let projectEntryPath = reduxProject.entry_path;
   if (projectEntryPath[0] !== "/") {
     projectEntryPath = "/" + reduxProject.entry_path;
   }
 
   return projectEntryPath;
-}
-
-
+};
 
 //search project
-export const getSearchProject = (searchValue: string, projectList: ReduxProject[])=>{
-
+export const getSearchProject = (
+  searchValue: string,
+  projectList: ReduxProject[]
+) => {
   // let projectList = getProjects(state);
-  if(!searchValue){
+  console.log("searchValue: ", searchValue);
+  if (!searchValue) {
     return projectList;
-  }else{
-    const searchValueRegex = new RegExp(searchValue)
-    let projectSearchResult = projectList.filter((project)=>project.title.match(searchValueRegex))
+  } else {
+    let projectSearchResult = projectList.filter((project) =>
+      project.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
     console.log(projectSearchResult);
     return projectSearchResult;
   }
-}
+};
